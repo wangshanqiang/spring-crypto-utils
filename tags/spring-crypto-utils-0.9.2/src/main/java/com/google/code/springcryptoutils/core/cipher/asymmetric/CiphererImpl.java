7e6f18aa5b68b7a796a0c064d559857b2f7a15ac
mirko.caserta <mirko.caserta@466949fc-158c-11df-a35d-4d785ab5a681>
2010-02-14 20:48:23 +0000

    [maven-release-plugin]  copy for tag spring-crypto-utils-0.9.2

---
+++ tags/spring-crypto-utils-0.9.2/src/main/java/com/google/code/springcryptoutils/core/cipher/asymmetric/CiphererImpl.java
@@ -0,0 +1,74 @@
+package com.google.code.springcryptoutils.core.cipher.asymmetric;
+
+import com.google.code.springcryptoutils.core.cipher.Mode;
+
+import javax.crypto.Cipher;
+import java.security.Key;
+
+/**
+ * The default implementation for performing asymmetric encryption/decryption
+ * with a static key.
+ *
+ * @author Mirko Caserta (mirko.caserta@gmail.com)
+ */
+public class CiphererImpl implements Cipherer {
+
+    private String algorithm = "RSA";
+    private Mode mode;
+    private Key key;
+
+    /**
+     * The asymmetric key algorithm. The default is RSA.
+     *
+     * @param algorithm the asymmetric key algorithm
+     */
+    public void setAlgorithm(String algorithm) {
+        this.algorithm = algorithm;
+    }
+
+    /**
+     * Sets the encryption/decryption mode.
+     *
+     * @param mode the encryption/decryption mode
+     */
+    public void setMode(Mode mode) {
+        this.mode = mode;
+    }
+
+    /**
+     * Sets the encryption key.
+     *
+     * @param key the encryption key
+     */
+    public void setKey(Key key) {
+        this.key = key;
+    }
+
+    /**
+     * Encrypts/decrypts a message based on the underlying mode of operation.
+     *
+     * @param message if in encryption mode, the clear-text message, otherwise
+     *                the message to decrypt
+     * @return if in encryption mode, the encrypted message, otherwise the
+     *         decrypted message
+     * @throws AsymmetricEncryptionException on runtime errors
+     * @see #setMode(Mode)
+     */
+    public byte[] encrypt(byte[] message) {
+        try {
+            final Cipher cipher = Cipher.getInstance(algorithm);
+            switch (mode) {
+                case ENCRYPT:
+                    cipher.init(Cipher.ENCRYPT_MODE, key);
+                    break;
+                case DECRYPT:
+                    cipher.init(Cipher.DECRYPT_MODE, key);
+                    break;
+            }
+            return cipher.doFinal(message);
+        } catch (Exception e) {
+            throw new AsymmetricEncryptionException("error encrypting/decrypting message; mode=" + mode, e);
+        }
+    }
+
+}
---
+++ tags/spring-crypto-utils-0.9.2/src/main/java/com/google/code/springcryptoutils/core/keystore/KeyStoreFactoryBean.java
@@ -0,0 +1,66 @@
+package com.google.code.springcryptoutils.core.keystore;
+
+import org.springframework.beans.factory.FactoryBean;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.core.io.Resource;
+
+import java.security.KeyStore;
+
+/**
+ * A spring bean factory for instancing KeyStore objects.
+ *
+ * @author Mirko Caserta (mirko.caserta@gmail.com)
+ */
+public class KeyStoreFactoryBean implements FactoryBean, InitializingBean {
+
+    private Resource location;
+    private String password;
+    private String type = "JKS";
+
+    private KeyStore keystore;
+
+    /**
+     * Sets the keystore location.
+     *
+     * @param location the keystore location
+     */
+    public void setLocation(Resource location) {
+        this.location = location;
+    }
+
+    /**
+     * Sets the keystore password.
+     *
+     * @param password the keystore password
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * Sets the keystore type (defaults to JKS).
+     *
+     * @param type the keystore type (defaults to JKS)
+     */
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Object getObject() throws Exception {
+        return keystore;
+    }
+
+    public Class getObjectType() {
+        return KeyStore.class;
+    }
+
+    public boolean isSingleton() {
+        return true;
+    }
+
+    public void afterPropertiesSet() throws Exception {
+        keystore = KeyStore.getInstance(type);
+        keystore.load(location.getInputStream(), password.toCharArray());
+    }
+
+}
