1da982006d96c3470b9feb9da9ee2503f72c5793
mirko.caserta <mirko.caserta@466949fc-158c-11df-a35d-4d785ab5a681>
2011-02-09 15:09:11 +0000

    [maven-release-plugin]  copy for tag spring-crypto-utils-0.9.3

---
+++ tags/spring-crypto-utils-0.9.3/src/main/java/com/google/code/springcryptoutils/core/spring/cipher/symmetric/Base64EncodedSymmetricKeyGeneratorBeanDefinitionParser.java
@@ -0,0 +1,20 @@
+package com.google.code.springcryptoutils.core.spring.cipher.symmetric;
+
+import com.google.code.springcryptoutils.core.cipher.symmetric.Base64EncodedKeyGeneratorImpl;
+import org.springframework.beans.factory.support.BeanDefinitionBuilder;
+import org.springframework.beans.factory.xml.AbstractSingleBeanDefinitionParser;
+import org.w3c.dom.Element;
+
+public class Base64EncodedSymmetricKeyGeneratorBeanDefinitionParser extends AbstractSingleBeanDefinitionParser {
+
+    @Override
+    protected Class getBeanClass(Element element) {
+        return Base64EncodedKeyGeneratorImpl.class;
+    }
+
+    protected void doParse(Element element, BeanDefinitionBuilder bean) {
+        bean.addPropertyValue("algorithm", element.getAttribute("algorithm"));
+        bean.addPropertyValue("chunkOutput", element.getAttribute("chunkOutput"));
+    }
+
+}
